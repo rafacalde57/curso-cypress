16271894ed37761bb61d364da98274cccc94b96b
rafa <rafacalde57@gmail.com>
2024-02-26 13:08:07 -0300

    inhabilitar video post ejecucion

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   retries: 1,
+  video: false, //inhabilita la generacion del video luego de la ejecucion de los casos.. se cambia a true cuando se requiera
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
