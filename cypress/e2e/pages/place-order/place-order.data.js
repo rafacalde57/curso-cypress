1e4ef00e9c332a95a862ba1d18ce79310bb001b5
rafa <rafacalde57@gmail.com>
2024-02-26 12:41:51 -0300

    creacion de pagina place order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testData() {
+        return {
+            name: 'Test Name',
+            country: 'United States',
+            city: 'Boston',
+            creditCardNumber: '5555555555554444',
+            month: 12,
+            year: 2025,
+        };
+    }
+}
