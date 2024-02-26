f24dd14f7a9af9f52f146730a6c41dc94db92084
rafa <rafacalde57@gmail.com>
2024-02-26 11:45:05 -0300

    creacion archivos homepage

---
+++ cypress/e2e/pages/home/home.elements.js
@@ -0,0 +1,19 @@
+export class HomeElements{
+    static get categoriesMenu(){
+        return{
+            get phones(){
+                return cy.contains("a", "Phones");
+            },
+            get laptos(){
+                return cy.contains("a", "Laptops");
+            },
+            get monitors(){
+                return cy.contains("a", "Monitors");
+            },
+        };
+    }
+    
+    static product(productName){
+        return cy.contains("a", productName);
+    }
+}
