86c2febeae8013e2cca7d71f2b71cb776ddf63d3
rafa <rafacalde57@gmail.com>
2024-02-26 11:06:29 -0300

    fix reports & flakiness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+import { LoginMethods } from "./pages/login/login.mehods";
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const username = 'random01';
+    const password = 'random01';
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('#login2').click()
+    LoginMethods.login(username, password)
+    cy.get('#nameofuser').should('contain.text', username)
+    cy.wait(5000) 
   })
 })
