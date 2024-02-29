0575c84e0d10a409f5fffb0408eb5aabf1cd679d
rafa <rafacalde57@gmail.com>
2024-02-29 10:30:38 -0300

    caso de prueba " Inicio de sesión válido"

--- cypress/e2e/pages/common-page/common-page.methods.js
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -47,4 +47,8 @@ export class CommonPageMethods {
         }
         return result;
     }
+
+    static verifySignedUser(username) {
+        CommonPageElements.signedUser.should('have.text', `Welcome ${username}`);
+    }
 }
--- cypress/e2e/tests/registro.cy.js
+++ cypress/e2e/tests/registro.cy.js
@@ -1,12 +1,13 @@
 import { CommonPageData } from "../pages/common-page/common-page.data"
 import { CommonPageMethods } from "../pages/common-page/common-page.methods"
+import { LoginData } from "../pages/login/login.data"
 import { SignupElements } from "../pages/signup/signup.elements"
 import { SignupMethods } from "../pages/signup/signup.methods"
 import { Logger } from "../util/logger"
 
 const user = CommonPageMethods.generateRandomString();
 const password = CommonPageMethods.generateRandomString(7);
-const existingUser = "Test01"
+const existingUser = LoginData.validCredentials.username
 
 describe(CommonPageData.testSuites.registro, ()=>{
     it("Registro de un usuario válido", ()=>{
