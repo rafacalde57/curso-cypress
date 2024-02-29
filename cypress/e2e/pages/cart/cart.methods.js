2a1a0c83878930468db86200a3c2155b213faa34
rafa <rafacalde57@gmail.com>
2024-02-29 11:35:19 -0300

    caso de prueba "Agregar producto al carrito"

--- cypress/e2e/pages/cart/cart.methods.js
+++ cypress/e2e/pages/cart/cart.methods.js
@@ -4,4 +4,8 @@ export class CartMehods {
     static clickOnDeleteLink(productName){
         CartElements.links.delete(productName).click();
     }
+
+    static verifyProductAdded(productName) {
+        CartElements.links.delete(productName).should('be.visible');
+    }
 }
--- cypress/e2e/tests/catalogo-y-compras.cy.js
+++ cypress/e2e/tests/catalogo-y-compras.cy.js
@@ -1,11 +1,14 @@
+import { CartMehods } from "../pages/cart/cart.methods";
 import { CommonPageData } from "../pages/common-page/common-page.data";
 import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { HomeMethods } from "../pages/home/home.methods";
 import { LoginData } from "../pages/login/login.data";
 import { LoginMethods } from "../pages/login/login.mehods";
+import { ProductDetailsMethods } from "../pages/product-details/product-details.methods";
 import { Logger } from "../util/logger";
 
 const user = LoginData.validCredentials;
+const product = 'ASUS Full HD'
 
 describe(CommonPageData.testSuites.catalogoYCompras, () => {
     it('Navegación por categorías', () => {
@@ -29,4 +32,44 @@ describe(CommonPageData.testSuites.catalogoYCompras, () => {
         HomeMethods.verifyProductDisplayed('Apple monitor 24')
         HomeMethods.verifyProductDisplayed('ASUS Full HD')
     })
+
+    it('Agregar producto al carrito', () => {
+
+        Logger.stepNumber(1);
+        Logger.step('Iniciar sesión como usuario registrado.')
+        Logger.subStep('Navigate to DemoBlaze page')
+        CommonPageMethods.naviateToDemoPage()
+        Logger.subStep('Click on "Log In" option')
+        CommonPageMethods.clickOnLoginOption()
+        LoginMethods.login(user.username, user.password)
+
+        Logger.stepNumber(2);
+        Logger.step('Navegar a la página de inicio.')
+        CommonPageMethods.clickOnHomeOption()
+
+        Logger.stepNumber(3);
+        Logger.step('Seleccionar una categoría de productos en el menú de navegación.')
+        HomeMethods.clickOnMonitorsOption()
+
+        Logger.stepNumber(4);
+        Logger.step('Hacer clic en un producto específico')
+        HomeMethods.clickOnProductLink(product)
+
+        Logger.stepNumber(5);
+        Logger.step('Verificar que se muestra la página de detalles del producto')
+        ProductDetailsMethods.verifyProductDetailsPageDisplayed()
+
+        Logger.stepNumber(6);
+        Logger.step('Hacer clic en el botón "Add to cart".')
+        ProductDetailsMethods.clickOnAddToCartButton()
+
+        Logger.stepNumber(7);
+        Logger.step(' Verificar que se muestra un mensaje de confirmación y el producto se agrega al carrito.')
+        ProductDetailsMethods.verifyProductAddedMessage();
+        CommonPageMethods.clickOnCartOption();
+        CartMehods.verifyProductAdded(product, {timeout: 30000})
+
+
+    })
+
 });
